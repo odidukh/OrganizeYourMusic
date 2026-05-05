2c51963d5173eac75ed49999e10cbdedce3be02a
odidukh <oles.didukh@gmail.com>
2026-05-05 16:10:33 +0300

    chore: install rewrite deps and configure base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/OrganizeYourMusic/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     port: 8000,
-  }
+  },
 })
